1a2d44bd97dfb3ed0aeb829e5e05e4d3857b8020
carlos-cg-tec <a01665778@tec.mx>
2025-09-19 16:00:08 -0600

    se agregaron comentarios de las nuevas funciones

--- paint.py
+++ paint.py
@@ -35,7 +35,7 @@ def square(start, end):
 
     end_fill()
 
-
+#funcion nueva para dibujar un circulo
 def circle(start, end):
     """Draw circle from start to end."""
     scale = 0.01
@@ -50,6 +50,7 @@ def circle(start, end):
 
     end_fill()
 
+#funcion nueva para dibujar un hexagono
 def hexagon(start, end):
     up()
     goto(start.x, start.y)
@@ -62,6 +63,7 @@ def hexagon(start, end):
 
     end_fill()
 
+#funcion nueva para dibujar un rectangulo
 def rectangle(start, end):
     """Draw rectangle from start to end."""
     up()
@@ -121,7 +123,7 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
-onkey(lambda: color('purple') 'P')
+onkey(lambda: color('purple') 'P') #se agrego el color morado
 onkey(lambda: store('shape', hexagon), 'h')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
